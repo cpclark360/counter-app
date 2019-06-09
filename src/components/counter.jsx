2ad50cba55568fccfbce93463aa1c350bb2cf09b
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-06-09 03:55:09 -0500

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,18 +6,41 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  // constructor() {
+  //   super();
+  //   // binds 'this' to Counter class
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  // Arrow functions inherit 'this'
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
         <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
   }
